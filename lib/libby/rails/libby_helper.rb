0beca225093dbfb8db8491812717d4d5e3ef607b
Derek Lindahl <dlindahl@customink.com>
2010-10-12 15:53:09 -0400

    0.0.9 - Added ability to force the inclusion of a library.
    This is particularly useful if you have a layout that is including the library at the top of the page while a view is including it at the bottom via a library with a core component. Since the layout is rendered last, the library is never included at the top of the page and that can cause unexpected results.

--- lib/libby/rails/libby_helper.rb
+++ lib/libby/rails/libby_helper.rb
@@ -9,35 +9,36 @@ module Libby::Rails::LibbyHelper
   # [version] Optional. If an instance of Libby::JavascriptLibrary is provided, the version will automatically be checked
   # [return] Boolean
   # ---
-  def library_included?( lib_name, version = nil )
+  def library_included?( lib, version = nil )
     @included_libraries ||= {}
-    
-    if lib_name.is_a? Libby::JavascriptLibrary
-      version = lib_name.version
-      lib_name = lib_name.name
-    end
 
-    if @included_libraries.has_key? lib_name
+    if @included_libraries.has_key? lib.name
       # Check the version of the included library as well (if provided)
-      (version) ? @included_libraries[lib_name].include?( version ) : true
+      (version) ? @included_libraries[lib.name].include?( version ) : true
     end
   end
 
   def include_js_library( library_name, *args )
+    options = args.extract_options!
+
+    force_include = options.delete(:force_include) if options.has_key? :force_include
+
     lib = compose_library( library_name, *args )
 
     # If the library exists and has not already been included, include it. This helps prevents views from including a library more than one time on a page.
-    if lib and not library_included? lib
+    if lib and ( force_include or not library_included?(lib) )
       add_included_library( lib )
       files_to_include = lib.include
-      javascript_include_tag *files_to_include unless files_to_include.empty?
+      if files_to_include and not files_to_include.empty?
+        javascript_include_tag *files_to_include
+      end
     end
   end
 
   def method_missing( method_id, *args )
     if match = /^include_(\w+)/.match(method_id.to_s)
       library_name = match.captures.first
-      include_js_library library_name, *args
+      include_js_library( library_name, *args )
     else
       super
     end
